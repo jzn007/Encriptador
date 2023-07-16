860ecaac8d9a3767ab01348ffbf218995797226a
jzn007 <jzn007@hotmail.com>
2023-07-16 01:57:51 -0600

    Encriptar y Desencriptar en JavaScript

--- js/encriptador.js
+++ js/encriptador.js
@@ -9,7 +9,7 @@ var dictionary = {
 var mensajeDesencriptado = "felicidades por enfrentar este desafio y haberlo concluido con exito!";
 var mensajeEncriptado = "fenterlimescimesdaidenters poberr enternfrenterntair enterstenter dentersaifimesober y haibenterrlober cobernclufatimesdober cobern enterximestober!";
 
-const encrypt = (message) =>{
+const encryptMessage = (message) =>{
     for(const [key, value] of Object.entries(dictionary)){
         message = message.replaceAll(key, value);
     }
@@ -17,28 +17,12 @@ const encrypt = (message) =>{
 };
 
 
-const decrypt = (message) => {
+const decryptMessage = (message) => {
     for(const [key, value] of Object.entries(dictionary)){
         message = message.replaceAll(value, key);
     }
     return message;
 }
-
-
-// function encriptarMensaje(mensaje){
-//     for(const [key, value] of Object.entries(dictionary)){
-//         mensaje = mensaje.replaceAll(key, value);
-//     }
-//     return mensaje;
-// }
-
-// function desencriptarMensaje(mensaje){
-//     for(const [key, value] of Object.entries(dictionary)){
-//         mensaje = mensaje.replaceAll(value, key);
-//     }
-        
-//     return mensaje;
-// }
 
 const main = () => {
     mensajeD = decrypt(mensajeEncriptado);
@@ -52,4 +36,59 @@ const main = () => {
         console.log("No son iguales");
 }
 
-main();
+const hideMessageNotFoud = (hide) => {
+    let textAreaResult = document.getElementsByClassName("textarea-result")[0];
+    let labelNotfoundResult = document.getElementsByClassName("label-notfound-result")[0];
+    let labelIngresatextoResult = document.getElementsByClassName("label-ingresatexto-result")[0];
+    let buttonCopyResult = document.getElementsByClassName("buttonCopy-result")[0];
+
+    textAreaResult.style.backgroundImage = hide ? "none" : "url('/img/muneco.png')";
+    labelNotfoundResult.style.visibility = hide ? "hidden" : "visible";
+    labelIngresatextoResult.style.visibility = hide ? "hidden" : "visible";
+    buttonCopyResult.style.visibility = hide ? "visible" : "hidden";
+}
+
+const copyMessageResultToClipboard = () => {
+    navigator.clipboard.write("");
+}
+
+const encrypt = (yes) => {
+    let textAreaProcess = document.getElementsByClassName("textarea-process")[0];
+    let textAreaResult = document.getElementsByClassName("textarea-result")[0];
+    debugger;
+    if(textAreaProcess.value.length === 0){
+        alert("Debes escribir un mensaje para " + (yes ? "Encriptar" : "Desencriptar"));
+        hideMessageNotFoud(false);
+    } else {
+        hideMessageNotFoud(true);
+        textAreaResult.value = yes ? encryptMessage(textAreaProcess.value) : decryptMessage(textAreaProcess.value);
+        textAreaProcess.value="";
+    }
+}
+
+
+const copy = () => {
+    let textAreaProcess = document.getElementsByClassName("textarea-process")[0];
+    let textAreaResult = document.getElementsByClassName("textarea-result")[0];
+
+    textAreaProcess.value =textAreaResult.value;
+    hideMessageNotFoud(false);
+    textAreaResult.value="";
+}
+// const decrypt = () => {
+//     debugger;
+//     let textAreaProcess = document.getElementsByClassName("textarea-process")[0];
+//     let textAreaResult = document.getElementsByClassName("textarea-result")[0];
+    
+//     if(textAreaProcess.value.length === 0){
+//         alert("Debes escribir un mensaje");
+//         hideMessageNotFoud(true);
+//     } else {
+//         textAreaResult.value = encryptMessage(textAreaProcess.value);
+//         hideMessageNotFoud(true);
+//     }
+// }
+
+
+hideMessageNotFoud(false);
+//main();
